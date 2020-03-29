c2c2a6cce5003a8a750182720c91c9ba50f4f283
kim_dy <playkimdy@gmail.com>
2020-03-29 23:03:44 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+import discord
+from discord.ext import commands
+import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+json_file_name = '/users/user/desktop/heroic-venture-270306-6b725eb305d4.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1FxYkmYVxMRHXk1VrBgVk0PBrKlzKVFsQZHw4fDdSbis/edit#gid=0'
+doc = gc.open_by_url(spreadsheet_url)
+
+client = commands.Bot(command_prefix = '>')
+token = 'Njg4ODkxNTU2MjExODUxMzI5.Xnmhnw.XcUxXURqyGpRtVFRP8IZlCw1imI'
+
+@client.event
+async def on_ready () :
+    print ("Bot is working")
+    print (client.user.id)
+
+@client.event
+async def on_guild_join(guild) :
+    doc.add_worksheet(str(guild.id), rows = '5', cols = '10')
+
+@client.command(aliaes = ['핑'])
+async def ping (ctx) :
+    latency = round(client.latency, 4)
+    embed = discord.Embed(colour = discord.Colour.blue(), description = '{}ms'.format(latency))
+    embed.set_author (name = 'Pong!')
+    embed.set_thumbnail(url = 'https://cdn2.iconfinder.com/data/icons/sport-8/70/ping_pong-512.png')
+    embed.set_footer (text = datetime.datetime.today())
+    await ctx.channel.send (embed = embed)
+
+startup_extensions = ['Internet','helpcommand', 'userinfo']
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            client.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+client.run(token)
+
